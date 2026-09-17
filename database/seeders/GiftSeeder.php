4144c90f647daffc3ae7522b9d2f49b8a308eeec
Wladimirandrea <59346479+Wladimirandrea@users.noreply.github.com>
2026-09-17 13:01:30 -0500

    Update GiftSeeder.php

--- database/seeders/GiftSeeder.php
+++ database/seeders/GiftSeeder.php
@@ -7,37 +7,34 @@ use Illuminate\Support\Facades\DB;
 
 class GiftSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $gifts = [
-            [
-                'name' => 'Pañales',
-                'max_selection' => 10,
-                'image_url' => 'https://media.istockphoto.com/id/541001932/es/foto/pa%C3%B1ales-apilados-aislados-sobre-fondo-blanco.jpg?s=1024x1024&w=is&k=20&c=HFgAhJb3yr0WVM5uRNTm9bvVlkbA8eql9hsxa0Big6I=',
-                'guest_id' => null,
-            ],
-            [
-                'name' => 'Toallitas humedas',
-                'max_selection' => 1,
-                'image_url' => 'https://i5.walmartimages.com/seo/Huggies-Aloe-Vitamin-E-Baby-Wipes-for-Sensitive-Skin-Unscented-1-Flip-Top-Pack-176-Wipes_892a867f-9d6b-425c-94d6-7421640c85c5.776ae215da716f05056fc0d3fec76b6e.jpeg?odnHeight=573&odnWidth=573&odnBg=FFFFFF
-                ',
-                'guest_id' => null,
-            ],
-            [
-                'name' => 'Set de biberones',
-                'max_selection' => 1,
-                'image_url' => 'https://http2.mlstatic.com/D_NQ_NP_902095-MLA99463965546_112025-O.webp',
-                'guest_id' => null,
-            ],
-            [
-                'name' => 'Ropa de recién nacido',
-                'max_selection' => 5,
-                'image_url' => null,
-                'guest_id' => null,
-            ],
+            ['name' => 'Bañera', 'max_selection' => 1, 'image_url' => null],
+            ['name' => 'Panales', 'max_selection' => 10, 'image_url' => 'https://media.istockphoto.com/id/541001932/es/foto/pa%C3%B1ales-apilados-aislados-sobre-fondo-blanco.jpg?s=1024x1024&w=is&k=20&c=HFgAhJb3yr0WVM5uRNTm9bvVlkbA8eql9hsxa0Big6I='],
+            ['name' => 'Toallitas húmedas', 'max_selection' => 10, 'image_url' => null],
+            ['name' => 'Ropa de niña 0-3 meses', 'max_selection' => 5, 'image_url' => null],
+            ['name' => 'Ropa de niña 3-6 meses', 'max_selection' => 5, 'image_url' => null],
+            ['name' => 'Kit de cuidado personal bebe', 'max_selection' => 2, 'image_url' => null],
+            ['name' => 'Tetero Anti cólicos', 'max_selection' => 4, 'image_url' => null],
+            ['name' => 'Car seat', 'max_selection' => 1, 'image_url' => null],
+            ['name' => 'Cuna', 'max_selection' => 1, 'image_url' => null],
+            ['name' => 'Mecedora', 'max_selection' => 1, 'image_url' => null],
+            ['name' => 'Panales de algodón', 'max_selection' => 5, 'image_url' => null],
+            ['name' => 'Cobijas', 'max_selection' => 3, 'image_url' => null],
+            ['name' => 'Ropa invierno', 'max_selection' => 4, 'image_url' => null],
+            ['name' => 'Set de colonia', 'max_selection' => 2, 'image_url' => null],
+            ['name' => 'Crema corporal', 'max_selection' => 3, 'image_url' => null],
+            ['name' => 'Jabones bebe', 'max_selection' => 5, 'image_url' => null],
+            ['name' => 'Champú bebe', 'max_selection' => 3, 'image_url' => null],
+            ['name' => 'Toallas de baño', 'max_selection' => 3, 'image_url' => null],
+            ['name' => 'Kit postparto', 'max_selection' => 1, 'image_url' => null],
+            ['name' => 'Pañalera', 'max_selection' => 1, 'image_url' => null],
+            ['name' => 'Esponjas absorbente lactancia', 'max_selection' => 3, 'image_url' => null],
+            ['name' => 'Porta bebe', 'max_selection' => 1, 'image_url' => null],
+            ['name' => 'Lazos', 'max_selection' => 5, 'image_url' => null],
+            ['name' => 'Almohada lactancia', 'max_selection' => 1, 'image_url' => null],
+            ['name' => 'Muñequitos de apego', 'max_selection' => 3, 'image_url' => null],
         ];
 
         foreach ($gifts as $gift) {
@@ -46,7 +43,6 @@ class GiftSeeder extends Seeder
                 [
                     'max_selection' => $gift['max_selection'],
                     'image_url'     => $gift['image_url'],
-                    'guest_id'      => $gift['guest_id'],
                     'created_at'    => now(),
                     'updated_at'    => now(),
                 ]
