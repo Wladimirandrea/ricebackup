449e934000d949196d352bb1dc22fe74a54fd4fc
Wladimir <wladimirandrea2@gmail.com>
2026-09-17 12:22:35 -0500

    feat: agregar GiftSeeder y GiftGuestSeeder para producción

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             ScheduleSeeder::class,
             GuestSeeder::class,
             GiftSeeder::class,
+            GiftGuestSeeder::class,
         ]);
          
     }
---
+++ database/seeders/GiftGuestSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GiftGuestSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Asignaciones iniciales de regalos elegidos por invitados
+        $assignments = [
+            [
+                'gift_id'  => 1, // ID del regalo (ej. Pañales)
+                'guest_id' => 1, // ID del invitado
+            ],
+        ];
+
+        foreach ($assignments as $assignment) {
+            DB::table('gift_guest')->updateOrInsert(
+                [
+                    'gift_id'  => $assignment['gift_id'],
+                    'guest_id' => $assignment['guest_id'],
+                ],
+                [
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]
+            );
+        }
+    }
+}
